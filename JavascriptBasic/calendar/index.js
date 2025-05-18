91729583baf1843662564f1b7b097a3374fa94f2
najmshigri <najmshigri@gmail.com>
2025-05-18 15:52:26 +0500

    Added calendar project files

---
+++ JavascriptBasic/calendar/index.js
@@ -0,0 +1,24 @@
+ const date = document.getElementById("date");
+    const day = document.getElementById("day");
+    const month = document.getElementById("month");
+    const year = document.getElementById("year");
+
+    const today = new Date();
+
+    console.log(today);
+
+    const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    const allmonths = [ "January", "February", "March", "April", "May", "June",
+                "July", "August", "September", "October", "November", "December"
+            ];
+
+
+    date.innerHTML = (today.getDate()<10?"0":"") + today.getDate();
+    day.innerHTML = weekdays[today.getDay()];
+    month.innerHTML = allmonths[today.getMonth()];
+    year.innerHTML = today.getFullYear();
+
+
+
+
